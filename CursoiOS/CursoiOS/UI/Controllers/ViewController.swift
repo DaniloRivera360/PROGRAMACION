c3f5ae821f42e63f550e82af9d631418c06cea3b
Emerson Danilo <Serrano Rivera>
2019-09-24 20:27:43 +0200

    cambios

--- CursoiOS/CursoiOS/UI/Controllers/ViewController.swift
+++ CursoiOS/CursoiOS/UI/Controllers/ViewController.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var labelName: UILabel?
-
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        
+        // se imprime un mensaje al presionar el boton.
+        print("Button Pressed")
+        // se cambia el texto del label al presionar el boton
+        labelName.text = "He pulsado el boton"
+        labelName.textAlignment = .center
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        actionButton.setTitle("MI BOTON", for: .normal)
+        
     }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear(false)
+        labelName.text = "JUMP"
+    
+        
+    }
+    
 
 
 }
